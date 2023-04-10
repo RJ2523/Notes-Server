0204ca72a1d300f0b768a202ca4d4cdd04be9ec4
snehaljha <snehaljha08@gmail.com>
2023-04-11 02:22:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const config = require('./config.json');
+
+const express = require('express');
+
+const server = express();
+
+const notesRouter = require('./api/notes');
+
+server.use('/notes', notesRouter);
+
+server.listen(config.port, () => 
+    console.log(`Server listening on port ${config.port}`));
